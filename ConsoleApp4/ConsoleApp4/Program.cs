f1eb8c65cc8862d557422c63e6691e1cc68599b9
Gayathrimenon123 <120583142+Gayathrimenon123@users.noreply.github.com>
2023-09-05 11:29:04 +0530

    Add files via upload

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System;
+class program
+{
+    static void Main(string[] args)
+    {
+        /* string path = @"H:\TestData";
+         Directory.CreateDirectory(path);
+         string[] a = Directory.GetDirectories(path);
+         for (int i = 0; i < a.Length; i++)
+         {
+             Console.WriteLine(a[i]);
+         }
+         string path = @"H:\TestData\file.txt";
+         Console.WriteLine(File.Exists(path) ? "file exists" : "file does not exist");*/
+     
+        string pathName2 = @"H:\TestData\file3.txt";
+        Console.WriteLine(File.ReadAllText(pathName2));
+    }
+}
+
