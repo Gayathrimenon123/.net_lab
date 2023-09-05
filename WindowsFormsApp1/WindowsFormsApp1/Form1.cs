c94c80eceb8c8b065b05ced0ef07a51f2dd4c0c8
Gayathrimenon123 <120583142+Gayathrimenon123@users.noreply.github.com>
2023-09-05 11:27:20 +0530

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public static String rno, name,course;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            comboBox1.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            rno=textBox1.Text;
+            name=textBox2.Text;
+            course=comboBox1.Text;
+            
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+        
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            label1.Text = Form1.rno;
+            label2.Text = Form1.name;
+            label3.Text = Form1.course;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if(textBox1.Text=="generate") {
+                Button b1=new Button();
+                Button b2=new Button();
+                b1.SetBounds(20, 30, 100, 30);
+                b1.Text = "b1";
+                b1.Enabled = true;
+                this.Controls.Add(b1);
+                b2.SetBounds(40,30,100,30);
+                b2.Text = "b2";
+                b2.Enabled = true;
+                this.Controls.Add(b2);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int n=Convert.ToInt32(comboBox1.Text);
+            if(comboBox1.BackColor ==Color.Cyan) {
+                //richTextBox1.Text = "hai";
+               richTextBox1.BackColor = Color.Cyan;
+                //richTextBox1.Size = new System.Drawing.Size(50, 50);
+            }
+            else
+            {
+                richTextBox1.BackColor = Color.Black;
+            }
+        }
+
+       
+    }
+}
